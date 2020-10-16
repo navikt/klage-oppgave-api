6521dd9732eb94399b195a1c4ff815ed443bf948
Hågen Hasle <haagenhasle@gmail.com>
2020-10-16 15:12:58 +0200

    Lagt til nye endepunkter for å kunne returnere ikke-tildelte og tildelte oppgaver, samt støtte for å hente dette gjennom filtrering mot Oppgave-systemet

--- src/main/kotlin/no/nav/klage/oppgave/api/OppgaveController.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/OppgaveController.kt
@@ -3,15 +3,18 @@ package no.nav.klage.oppgave.api
 import no.nav.klage.oppgave.config.SecurityConfiguration.Companion.ISSUER_AAD
 import no.nav.klage.oppgave.domain.Tilganger
 import no.nav.klage.oppgave.domain.view.OppgaveView
+import no.nav.klage.oppgave.service.OppgaveSearchCriteria
 import no.nav.klage.oppgave.service.OppgaveService
+import no.nav.klage.oppgave.service.unleash.TokenUtils
 import no.nav.klage.oppgave.util.getLogger
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @ProtectedWithClaims(issuer = ISSUER_AAD)
-class OppgaveController(val oppgaveService: OppgaveService) {
+class OppgaveController(val oppgaveService: OppgaveService, val tokenUtils: TokenUtils) {
 
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
@@ -27,6 +30,35 @@ class OppgaveController(val oppgaveService: OppgaveService) {
     fun getOppgaver(): List<OppgaveView> {
         logger.debug("getOppgaver is requested")
         return oppgaveService.getOppgaver()
+    }
+
+    @GetMapping("/oppgaver/ikketildelt")
+    fun findIkkeTildelteOppgaver(): List<OppgaveView> {
+        logger.debug("findIkkeTildelteOppgaver is requested")
+        return oppgaveService.searchOppgaver(OppgaveSearchCriteria(erTildeltSaksbehandler = false))
+    }
+
+    @GetMapping("/oppgaver/tildelt/{ident}")
+    fun findTildelteOppgaver(@PathVariable(name = "ident", required = false) ident: String?): List<OppgaveView> {
+        logger.debug("findTildelteOppgaver is requested")
+        return oppgaveService.searchOppgaver(
+            OppgaveSearchCriteria(
+                erTildeltSaksbehandler = true,
+                saksbehandler = ident
+            )
+        )
+    }
+
+    @GetMapping("/oppgaver/tildelt/me")
+    fun findMineTildelteOppgaver(): List<OppgaveView> {
+        logger.debug("findMineTildelteOppgaver is requested")
+        val ident = tokenUtils.getInnloggetIdent()
+        return oppgaveService.searchOppgaver(
+            OppgaveSearchCriteria(
+                erTildeltSaksbehandler = true,
+                saksbehandler = ident
+            )
+        )
     }
 
 }
--- src/main/kotlin/no/nav/klage/oppgave/clients/OppgaveClient.kt
+++ src/main/kotlin/no/nav/klage/oppgave/clients/OppgaveClient.kt
@@ -4,11 +4,14 @@ import brave.Tracer
 import no.nav.klage.oppgave.domain.gosys.BEHANDLINGSTYPE_FEILUTBETALING
 import no.nav.klage.oppgave.domain.gosys.BEHANDLINGSTYPE_KLAGE
 import no.nav.klage.oppgave.domain.gosys.OppgaveResponse
+import no.nav.klage.oppgave.service.OppgaveSearchCriteria
 import no.nav.klage.oppgave.util.getLogger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
+import org.springframework.web.util.UriBuilder
+import java.net.URI
 
 @Component
 class OppgaveClient(
@@ -45,6 +48,41 @@ class OppgaveClient(
             .retrieve()
             .bodyToMono<OppgaveResponse>()
             .block() ?: throw RuntimeException("Oppgaver could not be fetched")
+    }
+
+    fun searchOppgaver(oppgaveSearchCriteria: OppgaveSearchCriteria): OppgaveResponse {
+        logger.debug("Fetching oppgaver")
+
+        return oppgaveWebClient.get()
+            .uri { uriBuilder -> oppgaveSearchCriteria.buildUri(uriBuilder) }
+            .header("Authorization", "Bearer ${stsClient.oidcToken()}")
+            .header("X-Correlation-ID", tracer.currentSpan().context().traceIdString())
+            .header("Nav-Consumer-Id", applicationName)
+            .retrieve()
+            .bodyToMono<OppgaveResponse>()
+            .block() ?: throw RuntimeException("Oppgaver could not be fetched")
+    }
+
+    private fun OppgaveSearchCriteria.buildUri(uriBuilder: UriBuilder): URI {
+        var uriBuilder = uriBuilder
+            .queryParam("statuskategori", OppgaveClient.STATUSKATEGORI_AAPEN)
+
+        this.type?.let { uriBuilder = uriBuilder.queryParam("tema", mapType(it)) }
+        this.ytelse?.let { uriBuilder = uriBuilder.queryParam("tema", mapYtelseTilTema(it)) }
+        this.erTildeltSaksbehandler?.let { uriBuilder = uriBuilder.queryParam("tildeltRessurs", it) }
+        this.saksbehandler?.let { uriBuilder = uriBuilder.queryParam("tilordnetRessurs", it) }
+
+        return uriBuilder.build()
+    }
+
+    private fun mapType(type: String): String {
+        //TODO
+        return BEHANDLINGSTYPE_KLAGE
+    }
+
+    private fun mapYtelseTilTema(type: String): String {
+        //TODO
+        return TEMA_SYK
     }
 }
 
