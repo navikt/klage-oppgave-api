ebc0f3f321ed3ce1c8a5a92d25b3eb16e8bd145f
Richard Borge <richard.borge@nav.no>
2020-11-17 10:40:15 +0100

    Some code cleanup

--- src/main/kotlin/no/nav/klage/oppgave/service/HjemmelParsingService.kt
+++ src/main/kotlin/no/nav/klage/oppgave/service/HjemmelParsingService.kt
@@ -8,17 +8,15 @@ class HjemmelParsingService {
 
     private val hjemmelRegex = """(\d{1,2}-\d{1,2})+""".toRegex()
 
-    fun extractHjemmel(text: String): List<String> = hjemmelRegex.find(text).collect()
+    fun extractHjemmel(text: String): List<String> = hjemmelRegex.findAll(text).collect()
 
-    private fun MatchResult?.collect(): List<String> {
-        var matchResult = this
+    private fun Sequence<MatchResult>.collect(): List<String> {
         val list = mutableListOf<String>()
-        while (matchResult != null) {
-            val hjemmel = matchResult.value.replace("§", "").trim()
+        this.iterator().forEachRemaining {
+            val hjemmel = it.value.replace("§", "").trim()
             if (hjemmel.isValidHjemmel()) {
                 list.add(hjemmel)
             }
-            matchResult = matchResult.next()
         }
         return list
     }
